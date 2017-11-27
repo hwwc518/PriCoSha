bcee6ebaf881d9454ca201b34bdf48ec3dd581e6
Ashley Lee <al4596wk@gmail.com>
2017-11-26 23:07:25 -0500

    login authorization updated

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, flash, request, session, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('home.html')
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] == 'user' and request.form['password'] == 'password':
+        session['logged_in'] = True
+    else:
+        flash('password DOES NOT EXIST .__.')
+    return index()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
