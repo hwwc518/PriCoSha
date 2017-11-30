5c531c33bf9efcd919df9b81861c5390c2a3dfee
Hui Wah Chiang <hwwc518@gmail.com>
2017-11-30 08:47:55 -0500

    changed timeout

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ conn = pymysql.connect(host='localhost',
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=1)
+    app.permanent_session_lifetime = timedelta(minutes=30)
     # if 'logged_in' not in session:
     #     flash('Timed out, please login again', 'danger')
     #     return redirect(url_for('login'))
