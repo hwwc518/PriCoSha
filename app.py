be58747624fbe7b2b503bd593858f4efa7a83263
Ashley Lee <al4596wk@gmail.com>
2017-12-07 14:13:36 -0500

    updated post

--- app.py
+++ app.py
@@ -216,55 +216,23 @@ def groups():
     if 'logged_in' in session:
         # check for create group action
         #if request.form['mems']:
-        cursor = conn.cursor()
         creator = session['username']
         group_name = request.form['group_name']
         description = request.form['description']
-        mems = request.form['mems']
-        listMems = mems.split(', ')
-            # add creator first if not already in
-        query = 'SELECT COUNT(*) FROM Member WHERE username = %s AND group_name = %s'
-            # check logic / not sure if this works
-        if (cursor.execute(query, (creator, group_name)) == 0):
-            query = 'INSERT INTO Member (username, group_name, username_creator)\
-                VALUES(%s, %s, %s)'
-            query2 = 'INSERT INTO FriendGroup (group_name, username, description)\
-                VALUES(%s, %s, %s)'
-            cursor.execute(query, (creator, group_name, creator))
-            cursor.execute(query2, (group_name, creator, description))
-            conn.commit()
-            # for all members in query
-            invalidMems = []
-	    for mem in listMems:
-	        # verify if member is valid
-	        queryx = 'SELECT COUNT(*) FROM Person WHERE username = %s'
-            if (cursor.execute(queryx, mem) == 1):
-                query = 'INSERT INTO Member (username, group_name, username_creator)\
-                    VALUES(%s, %s, %s)'
-                query2 = 'INSERT INTO FriendGroup (group_name, username, description)\
-                    VALUES(%s, %s, %s)'
-                cursor.execute(query, (mem, group_name, creator))
-                cursor.execute(query2, (group_name, mem, description))
-                conn.commit()
-            else:
-                invalidMems.append(mem)
-            # close connection
-        cursor.close()
+        member = request.form['mems']
+        cursor = conn.cursor()
 
-            # if there were invalid members, flash them and redirect
-            ## todo: NOT DONE##
-            ###################
-            ###################
-            # if (invalidMems):
-            #     flash('')
-
-        return render_template('groups.html')    
-        # todo: add add member function
-        ###############################
-        ###############################
+        query = 'INSERT INTO FriendGroup (group_name, username, description)\VALUES(%s, %s, %s)'
+        
+        cursor.execute(query, (group_name, member, description))
+        conn.commit()
+        
+        return render_template('home.html')
     else:
         flash('Timed out, please login again', 'danger')
-        return redirect(url_for('login'))
+        #didn't work so replacing it temporarily
+        #return redirect(url_for('login'))
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
